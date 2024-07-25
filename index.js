bdfeacbbb1668bbbd62bfa930cba4836ec6c0cc2
rasulbeckov <rasulbekovakrom@gmail.com>
2024-07-25 15:25:44 +0500

    commit "initial commir"

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ const h2 = document.getElementById("h2");
 const h3 = document.getElementById("h3");
 
 function katta() {
-    const result = Number(number1.value) > Number(number2.value);
+    const result = Number(number1.value);
+    const result2 = Number(number2.value);
 
-    h1.innerHTML = (result)
+    if (result > result2) {
+        h1.innerHTML = result
+    }else{
+        h1.innerHTML = result2
+    }
+
 }
 
 function kichik() {
-    const result = Number(number1.value) < Number(number2.value);
+    const result = Number(number1.value);
+    const result2 = Number(number2.value);
 
-    h2.innerHTML = (result)
+    if (result2 > result) {
+        h2.innerHTML = result
+    }else{
+        h2.innerHTML = result2
+    }
+
 }
 
 function teng() {
